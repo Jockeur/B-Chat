33014ce9562c1a512875a1d9d5554ee3272b6442
NSI Coder <nsi-js-1g6@outlook.com>
2024-11-19 09:52:28 +0100

    Update Backend_Internet_Client_Side.py

--- Backend_Internet_Client_Side.py
+++ Backend_Internet_Client_Side.py
@@ -1,1 +1,11 @@
+import socket
+import threading
 
+def receive_messages(client socket):
+  while true:
+    try:
+      message = client_socket.recv(1024).decode('utf-8')
+      if message:
+        print(messsage)
+      else:
+        break
