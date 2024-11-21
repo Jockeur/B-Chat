15d78c3be97f78d9a6d251d0d09dfbac523408a7
Jockeur <jocceur@gmail.com>
2024-11-21 23:32:55 +0100

    fixing typos

--- Backend_Internet_Client_Side.py
+++ Backend_Internet_Client_Side.py
@@ -1,11 +1,13 @@
 import socket
 import threading
 
-def receive_messages(client socket):
-  while true:
+def receive_messages(client_socket):
+  while True:
     try:
       message = client_socket.recv(1024).decode('utf-8')
       if message:
-        print(messsage)
+        print(message)
       else:
         break
+    finally:
+      pass
