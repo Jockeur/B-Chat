bc8e082622dc84554a8feaceb8036ab2f174283a
Jockeur <jocceur@gmail.com>
2024-11-22 17:28:54 +0100

    sending and storing messages

--- client.py
+++ client.py
@@ -1,30 +1,30 @@
 import sys, socket
-import slogging
+from slogging import log
 
 # Créer une connection TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Se connecter au socket sur le port où le serveur écoute
 server_address = (sys.argv[1], 10000)
-slogging.log('Connecting to %s port %s' % server_address)
+log('Connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
 ## On peut envoyer et recevoir des data grâce à sock.sendall() et sock.recv() respectivement
 ## Comme pour le serveur
 try:
     message = input()
-    slogging.log('sending "%s"' % message)
+    log('sending "%s"' % message)
     sock.sendall(message.encode())
     
     # En attente de réponse
-    amount_recieved = 0
+    amount_received = 0
     amount_expected = len(message.encode())
     
-    while amount_recieved < amount_expected:
+    while amount_received < amount_expected:
         data = sock.recv(16)
-        amount_recieved += len(data)
-        slogging.log('recieved "%s"' % data.decode())
+        amount_received += len(data)
+        log('received "%s"' % data.decode())
         
 finally:
-    slogging.log('closing socket')
+    log('closing socket')
     sock.close()
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import socket
-import slogging
+from slogging import log
 import json
 import os
 
-
+messages={}
 with open(f'{os.getcwd()}/messages.json', 'r+') as file:
     messages = json.load(file)
 
@@ -12,7 +12,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-slogging.log('starting up on %s port %s' % server_address)
+log('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -21,27 +21,27 @@ sock.listen(1)
 while True:
     message=""
     # Wait for a connection
-    slogging.log('Waiting for a connection...')
+    log('Waiting for a connection...')
     connection, client_address = sock.accept()
     print()
     
     try:
-        slogging.log(f'Connection from {client_address}')
+        log(f'Connection from {client_address}')
         
         # Recieve the data in a small chunks and retransmit it
         while True:
             data = connection.recv(16).decode()
-            slogging.log('recieved "%s"' % data)
+            log('received "%s"' % data)
             if data:
-                slogging.log('Sending data back to the client')
+                log('Sending data back to the client')
                 connection.sendall(data.encode())
-                message += data.decode()
+                message += data
             else:
-                slogging.log(f'No more data from {client_address}')
+                log(f'No more data from {client_address}')
                 messages['test'] = message
                 with open(f'{os.getcwd()}/messages.json', 'r+') as file:
                     file.seek(0)
-                    file.dump(messages, file)
+                    json.dump(messages, file)
                     file.truncate()
                 break
         
